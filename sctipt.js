1c502814ae946e9601c77c2e65b876758d1f46b8
HussianMemon7576 <memonhussain753@gmail.com>
2026-01-12 14:36:14 +0500

    Add files via upload

---
+++ sctipt.js
@@ -0,0 +1,174 @@
+// let value = Number(prompt("Enter a number between 1 to 7"));
+//     switch (value) {
+//         case 1:
+//             document.write("Eat Apple");
+//             break;
+//         case 2:
+//             document.write("Eat Banana");
+//             break;
+//             case 3:
+//             document.write("Eat Mango");
+//             break;
+//         default:
+//             document.write("Please enter a valid number");
+//             break;
+//     }
+
+// let grade = String(prompt("Enter your grade"));
+// switch (grade) {
+//     case "A":
+//         document.write("Excellent");
+//         break;  
+//         case "B":
+//         document.write("Very Good");
+//         break;  
+//         case "C":
+//         document.write("Good");
+//         break;
+//         default:
+//         document.write("Please enter a valid grade");
+//         break;
+// }
+
+
+// let First = Number(prompt("Enter first number"));
+// let Second = Number(prompt("Enter second number"));
+// let Operator = prompt("Enter operator (+, -, *, /)");   
+// switch (Operator) {
+//     case "+":
+//         document.write(`The sum is: ${First + Second}`);    
+//         break;
+//     case "-":
+//         document.write(`The difference is: ${First - Second}`);
+//         break;
+//     case "*":
+//         document.write(`The product is: ${First * Second}`);
+//         break;
+//     case "/":
+//         document.write(`The quotient is: ${First / Second}`);
+//         break;
+//     default:
+//         document.write("Please enter a valid operator");
+//         break;
+// }
+
+// let month = Number(prompt("Enter month number (1-12)"));
+// switch (month) {
+//     case 1:
+//         document.write("January");
+//         break;
+//     case 2:
+//         document.write("February");
+//         break;
+//     case 3:
+//         document.write("March");
+//         break;
+//     case 4:
+//         document.write("April");
+//         break;
+//     case 5:
+//         document.write("May");
+//         break;
+//     case 6:
+//         document.write("June");
+//         break;
+//     case 7:
+//         document.write("July");
+//         break;
+//     case 8:
+//         document.write("August");
+//         break;
+//     case 9:
+//         document.write("September");
+//         break;
+//     case 10:
+//         document.write("October");
+//         break;
+//     case 11:
+//         document.write("November");
+//         break;
+//     case 12:
+//         document.write("December");
+//         break;
+//     default:
+//         document.write("Please enter a valid month number (1-12)");
+// }
+// let signal = prompt("Enter traffic signal color (red, yellow, green)");
+// switch (signal.toLowerCase()) {
+//     case "red":
+//         document.write("Stop");
+//         break;
+//     case "yellow":
+//         document.write("Slow Down");
+//         break;
+//     case "green":
+//         document.write("Go");
+//         break;
+//     default:
+//         document.write("Please enter a valid traffic signal color (red, yellow, green)");
+//         break;
+// }
+
+// let weekday = Number(prompt("Enter a number (1-7) to get the corresponding weekday"));
+// switch (weekday) {
+//     case 1: 
+//         document.write("Go for Work");
+//         break;      
+//     case 2:
+//         document.write("Attend Meetings");
+//         break;  
+//     case 3:
+//         document.write("Work on Projects");
+//         break;
+//     case 4:
+//         document.write("Team Collaboration");
+//         break;  
+//     case 5: 
+//     document.write("Wrap up Work");
+//     break;  
+//     case 6:
+//         document.write("Relax and Unwind");
+//         break;
+// case 7:
+//     document.write("Plan for the Week Ahead");
+//     break;
+//     default:
+//         document.write("Please enter a valid number (1-7)");
+//         break;
+// }
+// let first_dish = "Biryani";
+// let second_dish = "Pizza";
+// let third_dish = "Burger";
+// let order = prompt("Enter your order (Biryani, Pizza, Burger)");
+// switch (order) {
+//     case "Biryani":
+//         document.write(`You have ordered ${first_dish}`);   
+//         break;
+//     case "Pizza":
+//         document.write(`You have ordered ${second_dish}`);          
+//         break;  
+//     case "Burger":
+//         document.write(`You have ordered ${third_dish}`);          
+//         break;
+//     default:
+//         document.write("Sorry, we don't have that dish available.");
+//         break;
+// }
+// const Atm_option = "Please Enter a Card"
+// const second_option = "Please Enter Pin"
+// const third_option = "Please Select Transaction Type"
+// let atm = prompt("Enter ATM option (Please Enter a Card, Please Enter Pin, Please Select Transaction Type)");
+// switch (atm) {
+//     case "Please Enter a Card":
+//         document.write(`You have selected: ${Atm_option}`); 
+//         break;
+//     case "Please Enter Pin":
+//         document.write(`You have selected: ${second_option}`); 
+//         break;  
+//     case "Please Select Transaction Type":
+//         document.write(`You have selected: ${third_option}`); 
+//         break;
+//     default:
+//         document.write("Invalid ATM option selected.");
+//         break;
+// }
